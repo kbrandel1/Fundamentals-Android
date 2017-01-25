b7e8e5ae2b14233fcf08c43550ca5de5efa715c8
kbrandel1 <kbrandel1@student.cscc.edu>
2017-01-24 20:05:58 -0500

    week 2 exercise

--- src/com/kbrandel/Main.java
+++
@@ -1,8 +0,0 @@
-package com.kbrandel;
-
-public class Main {
-
-    public static void main(String[] args) {
-        // write your code here
-    }
-}
---
+++ src/com/kbrandel/week_02/Main.java
@@ -0,0 +1,23 @@
+package com.kbrandel.week_02;
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        String days[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        int temps[] = {45, 29, 10, 22, 41, 28, 33};
+        double probability[] = {0.95, 0.6, 0.25, 0.05, 0, 0.75, 0.9};
+
+        for (int i = 0; i < days.length; i++) {
+            if (temps[i] <= 32 && probability[i] > 0.5) {
+                System.out.println(days[i]);
+            }
+
+        }
+
+    }
+}
+
